1320f740444b29acd6c2f9d4004b013d07cc82a9
renegadebytes <info@renegadebytes.com>
2024-11-28 10:00:09 -0500

    Final Bookbot version: Reads and outputs book text and prints a report of the word count and frequency of letters used to the console.

--- main.py
+++ main.py
@@ -7,9 +7,18 @@ def main():
         # print to console 
         print(file_contents)
 
+        print(f"\n--- Beginning report of {path_to_file} ---")
+        #get word count
         count_words = word_count(file_contents)
-        print(f"The book contains {count_words} words.")
+        print(f"{count_words} words were found in the document.\n")
 
+        #get frequency of chars
+        count_char = letter_count(file_contents)
+        
+        for char, count in sorted(count_char.items()):
+            print(f"The '{char}' character was found {count} times.")
+        print("--- End of Report ---")
+        
     except FileNotFoundError:
         print(f"Error: '{path_to_file}' not found.")
     except Exception as e:
@@ -21,4 +30,20 @@ def word_count(text):
     words = text.split()
     return len(words)
 
+def letter_count(text):
+    
+    #this will make them all lowercase
+    text = text.lower()
+    
+    #dictionary for it to go to
+    char_freq = {}
+    
+    for char in text:
+        if char.isalpha():
+            if char in char_freq:
+                char_freq[char] += 1
+            else:
+                char_freq[char] = 1
+
+    return char_freq
 main()
